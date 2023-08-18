bb662b815965e310919a19d2457d84f4fd96d525
Jeriko <Jeriko8304@gmail.com>
2023-08-18 20:23:30 +0700

    change name

--- 03_Fundamental-Dart/Praktikum/Latihan/bin/tabung.dart
+++ 03_Fundamental-Dart/Praktikum/Latihan/bin/tabung.dart
@@ -2,13 +2,13 @@ import 'dart:io';
 import 'dart:math';
 
 void main() {
-  double silinder;
+  double si;
 
   print('masukkan nilai jari-jari');
   double j = double.parse(stdin.readLineSync()!);
   print('masukkan nilai tinggi');
   double t = double.parse(stdin.readLineSync()!);
 
-  silinder = pi * (j * j) * t;
-  print('nilai volume silinder anda adalah = $silinder');
+  si = pi * (j * j) * t;
+  print('nilai volume silinder anda adalah = $si');
 }
