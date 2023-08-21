855dea8c5ec8521e1844554cd8eeee234e6f6ba4
Jeriko <Jeriko8304@gmail.com>
2023-08-21 20:26:06 +0700

    add jam pasir

---
+++ 04_Branching-Looping-Function/Praktikum/latihan/bin/jam_pasir.dart
@@ -0,0 +1,22 @@
+import 'dart:io';
+
+void main() {
+  for (int i = 1; i <= 5; i++) {
+    for (int j = 1; j < i; j++) {
+      stdout.write(' ');
+    }
+    for (int k = 1; k <= 2 * (5 - i) + 1; k++) {
+      stdout.write('*');
+    }
+    print('');
+  }
+  for (int i = 5 - 1; i >= 1; i--) {
+    for (int j = 2; j <= i; j++) {
+      stdout.write(' ');
+    }
+    for (int k = 1; k <= 2 * (5 - i) + 1; k++) {
+      stdout.write('*');
+    }
+    print('');
+  }
+}
