b10fd076eeabde5020a0c98a5493b7e227052f3a
Jeriko <Jeriko8304@gmail.com>
2023-09-06 21:14:55 +0700

    add cupertino

---
+++ 10_Platform-Widget/Praktikum/Prioritas 1/myproject/lib/cupertino_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/cupertino.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CupertinoPageScaffold(
+      navigationBar: CupertinoNavigationBar(
+        middle: Text('CupertinoApp'),
+      ),
+      child: Center(
+        child: Text('This is CupertinoApp'),
+      ),
+    );
+  }
+}
