bf9472fb847054399e5b68e8954368fd113f9b56
Jeriko <Jeriko8304@gmail.com>
2023-10-16 21:21:13 +0700

    add new project to openai

---
+++ 24_OpenAI-In-Flutter/praktikum/lib/env/env.g.dart
@@ -0,0 +1,12 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'env.dart';
+
+// **************************************************************************
+// EnviedGenerator
+// **************************************************************************
+
+class _Env {
+  static const String apiKey =
+      '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';
+}
